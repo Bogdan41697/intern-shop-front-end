b43b707ca9c4c66321b8c9e6b1121beb69ca52f2
Bogdan41697 <bogdangrisko@gmail.com>
2018-08-28 15:58:32 +0300

    guards begin

---
+++ src/app/guards/index.ts
@@ -0,0 +1,5 @@
+import { RouteGuard } from './route.guard';
+
+export const guards: any[] = [RouteGuard];
+
+export * from './route.guard';
---
+++ src/app/guards/route.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { Go } from '../store';
+
+@Injectable()
+export class RouteGuard implements CanActivate {
+  constructor(private store: Store<State>) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | boolean {
+    const observable = this.store.select(isAuthenticated);
+
+    observable.subscribe(authenticated => {
+      if (!authenticated) {
+        this.store.dispatch(new Go({ path: ['/register'] }));
+      }
+    });
+
+    return observable;
+  }
+}
